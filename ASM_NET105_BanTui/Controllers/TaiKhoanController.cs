2fa7d871e8273f9158a0514939bf0286ee990657
Zero016-venom <vietanh.technol@gmail.com>
2024-05-12 22:05:11 +0700

    dang ky

---
+++ ASM_NET105_BanTui/Controllers/TaiKhoanController.cs
@@ -0,0 +1,62 @@
+﻿using ASM_NET105_BanTui.Core.Domain.IdentityEntities;
+using ASM_NET105_BanTui.DTO;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASM_NET105_BanTui.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public TaiKhoanController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterDTO registerDTO)
+        {
+            if (ModelState.IsValid == false)
+            {
+                ViewBag.Errors = ModelState.Values.SelectMany(temp => temp.Errors).Select(temp => temp.ErrorMessage);
+                return View(registerDTO);
+            }
+
+            ApplicationUser user = new ApplicationUser()
+            {
+                Email = registerDTO.Email,
+                PersonName = registerDTO.PersonName,
+                PhoneNumber = registerDTO.Phone,
+                UserName = registerDTO.Email
+            };
+
+            IdentityResult result = await _userManager.CreateAsync(user, registerDTO.Password);
+
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Index", "SanPham");
+            }
+            else
+            {
+                foreach (IdentityError item in result.Errors)
+                {
+                    ModelState.AddModelError("Register", item.Description);
+                }
+            }
+
+            return View(registerDTO);
+        }
+    }
+}
