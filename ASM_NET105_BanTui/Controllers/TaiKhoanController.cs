ed411b7d5b0eadff17b56f1a1d7bf70fed33bdfb
Zero016-venom <vietanh.technol@gmail.com>
2024-05-13 11:07:01 +0700

    view login & view register with role

--- ASM_NET105_BanTui/Controllers/TaiKhoanController.cs
+++ ASM_NET105_BanTui/Controllers/TaiKhoanController.cs
@@ -1,7 +1,9 @@
-﻿using ASM_NET105_BanTui.Core.Domain.IdentityEntities;
+﻿using ASM_NET105_BanTui.Core.Domain.Enums;
+using ASM_NET105_BanTui.Core.Domain.IdentityEntities;
 using ASM_NET105_BanTui.DTO;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
 
 namespace ASM_NET105_BanTui.Controllers
 {
@@ -9,9 +11,15 @@ namespace ASM_NET105_BanTui.Controllers
     {
         private readonly UserManager<ApplicationUser> _userManager;
 
-        public TaiKhoanController(UserManager<ApplicationUser> userManager)
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+
+        public TaiKhoanController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager,
+            RoleManager<ApplicationRole> roleManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
         }
 
         public IActionResult Index()
@@ -44,9 +52,36 @@ namespace ASM_NET105_BanTui.Controllers
 
             IdentityResult result = await _userManager.CreateAsync(user, registerDTO.Password);
 
-            if(result.Succeeded)
+            if (result.Succeeded)
             {
-                return RedirectToAction("Index", "SanPham");
+                //Kiem tra trang thai radio btn
+                if(registerDTO.UserType == Core.Domain.Enums.UserTypeOptions.Admin)
+                {
+                    //Tao role admin
+                    if (await _roleManager.FindByNameAsync(UserTypeOptions.Admin.ToString()) is null)
+                    {
+                        ApplicationRole applicationRole = new ApplicationRole()
+                        {
+                            Name = UserTypeOptions.Admin.ToString()
+                        };
+                        await _roleManager.CreateAsync(applicationRole);
+                    }
+
+                    //Them user moi trong role admin
+                    await _userManager.AddToRoleAsync(user, UserTypeOptions.Admin.ToString());
+                }
+                else
+                {
+                    //Tao role user
+                    if(await _roleManager.FindByNameAsync(UserTypeOptions.User.ToString()) is null)
+                    {
+                        ApplicationRole applicationRole = new ApplicationRole() { Name = UserTypeOptions.User.ToString() };
+                        await _roleManager.CreateAsync(applicationRole);
+                    }
+                    //Them user moi trong role user
+                    await _userManager.AddToRoleAsync(user, UserTypeOptions.User.ToString());
+                }
+                return RedirectToAction("Login", "TaiKhoan");
             }
             else
             {
@@ -57,6 +92,52 @@ namespace ASM_NET105_BanTui.Controllers
             }
 
             return View(registerDTO);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginDTO loginDTO)
+        {
+            if (ModelState.IsValid == false)
+            {
+                ViewBag.Errors = ModelState.Values.SelectMany(temp => temp.Errors).Select(temp => temp.ErrorMessage);
+                return View(loginDTO);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(loginDTO.Email, loginDTO.Password, 
+                isPersistent: false, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction(nameof(SanPhamController.Index));
+            }
+
+            ModelState.AddModelError("Login", "Email hoặc mật khẩu không hợp lệ");
+            return View(loginDTO);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(SanPhamController.Index));
+        }
+
+        public async Task<IActionResult> IsEmailAlreadyRegistered(string email)
+        {
+            ApplicationUser user = await _userManager.FindByEmailAsync(email);
+            if(user == null)
+            {
+                return Json(true);
+            }
+            else
+            {
+                return Json(false);
+            }
         }
     }
 }
